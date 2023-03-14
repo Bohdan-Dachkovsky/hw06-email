3ec0ae8c6385bd05726ebcd9451d24eec80e6e7a
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-03-14 19:17:21 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -21,9 +21,7 @@ function getContactById(textFile, contactId) {
 
       const parsedObj = JSON.parse(data)
 
-      const found = parsedObj.find((number) =>
-        console.log(Number(number.id) === contactId),
-      )
+      const found = parsedObj.find((number) => Number(number.id) === contactId)
       console.log(found)
     }
   })
