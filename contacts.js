0cc153ce986b863b1ba79478d9b6eb22a972d6bb
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-03-14 17:33:10 +0200

    Add nanoid

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const { uuid } = require('uuidv4')
+const Nanoid = require('nanoid')
 function listContacts(textFile) {
   fs.readFile(path.join(__dirname, textFile), 'utf8', function (error, data) {
     try {
@@ -49,10 +49,10 @@ function addContact(textFile, name, email, phone) {
       console.log('\nCurrent directory filenames:')
 
       const parsedObj = JSON.parse(data)
-
+      console.log(Nanoid.nanoid())
       parsedObj.map((number, idex, array) => {
         return array.push({
-          id: uuid().toString(),
+          id: Nanoid.nanoid(),
           name: name,
           email: email,
           phone: email,
