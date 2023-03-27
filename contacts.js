111c0bab90569ffe9329591ba66e22d7d056f2e7
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-03-27 17:34:39 +0300

    Commit

--- contacts.js
+++ contacts.js
@@ -45,7 +45,6 @@ function removeContact(contactId) {
         else {
           console.log('File written successfully\n')
           console.log('The written has the following contents:')
-          console.log(fs.readFileSync(textFile, 'utf8'))
         }
       })
       return console.log(newObject)
